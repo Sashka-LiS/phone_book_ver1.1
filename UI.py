234b709c8222156ed590fba9d276ed2c1fdb29c5
Alexandr Lisimenko <lisimenko.alexander@yandex.ru>
2023-03-23 14:03:24 +0300

    update UI.py

--- UI.py
+++ UI.py
@@ -1,35 +1,14 @@
 
 
 def render_menu(key_menu=None):
-    strings_for_all_menu = [
-                        "\n__________МЕНЮ__________", #0
-                        "1 - Добавить контакт", #1
-                        "2 - Показать телефонную книгу", #2
-                        "3 - Удалить контакт", #3
-                        "4 - Найти контакт", #4
-                        "5 - Редактировать контакт", #5
-                        "0 - Выход", #6
-                        "\n_______МЕНЮ ПОИСКА_______", #7
-                        "В каком столбе искать контакт?", #8
-                        "1 - Фамилия", #9
-                        "2 - Имя", #10
-                        "3 - Отчество", #11
-                        "4 - Email", #12
-                        "5 - Номер", #13
-                        "6 - Домашний номер", #14
-                        "7 - Рабочий номер", #15
-                        "0 - Отмена", #16
-                        "\n_____МЕНЮ РЕДАКТИРОВАНИЯ_____", #17
-                        "Какое поле отредактировать?" #18
-                        ]
     if key_menu == "main_menu":
-        strings_for_main_menu = [strings_for_all_menu[0],
-                                 strings_for_all_menu[1],
-                                 strings_for_all_menu[2],
-                                 strings_for_all_menu[3],
-                                 strings_for_all_menu[4],
-                                 strings_for_all_menu[5],
-                                 strings_for_all_menu[6]]
+        strings_for_main_menu = ["\n__________МЕНЮ__________",
+                                 "1 - Добавить контакт",
+                                 "2 - Показать телефонную книгу",
+                                 "3 - Удалить контакт",
+                                 "4 - Найти контакт",
+                                 "5 - Редактировать контакт",
+                                 "0 - Выход"]
         for string in strings_for_main_menu:
             print(string)
         existing_responses = [char for string in strings_for_main_menu for char in string if char.isdigit()]
@@ -39,16 +18,16 @@ def render_menu(key_menu=None):
         return response
 
     elif key_menu == "search_menu":
-        strings_for_search_menu = [strings_for_all_menu[7],
-                                   strings_for_all_menu[8],
-                                   strings_for_all_menu[9],
-                                   strings_for_all_menu[10],
-                                   strings_for_all_menu[11],
-                                   strings_for_all_menu[12],
-                                   strings_for_all_menu[13],
-                                   strings_for_all_menu[14],
-                                   strings_for_all_menu[15],
-                                   strings_for_all_menu[16]]
+        strings_for_search_menu = ["\n_______МЕНЮ ПОИСКА_______",
+                                   "В каком столбе искать контакт?",
+                                   "1 - Фамилия",
+                                   "2 - Имя",
+                                   "3 - Отчество",
+                                   "4 - Email",
+                                   "5 - Номер",
+                                   "6 - Домашний номер",
+                                   "7 - Рабочий номер",
+                                   "0 - Отмена"]
         for string in strings_for_search_menu:
             print(string)
         existing_responses = [char for string in strings_for_search_menu for char in string if char.isdigit()]
@@ -58,16 +37,16 @@ def render_menu(key_menu=None):
         return response
     
     elif key_menu == "edit_menu":
-        strings_for_edit_menu = [strings_for_all_menu[17],
-                                 strings_for_all_menu[18],
-                                 strings_for_all_menu[9],
-                                 strings_for_all_menu[10],
-                                 strings_for_all_menu[11],
-                                 strings_for_all_menu[12],
-                                 strings_for_all_menu[13],
-                                 strings_for_all_menu[14],
-                                 strings_for_all_menu[15],
-                                 strings_for_all_menu[16],]
+        strings_for_edit_menu = ["\n_____МЕНЮ РЕДАКТИРОВАНИЯ_____",
+                                 "Какое поле отредактировать?",
+                                 "1 - Фамилия",
+                                 "2 - Имя",
+                                 "3 - Отчество",
+                                 "4 - Email",
+                                 "5 - Номер",
+                                 "6 - Домашний номер",
+                                 "7 - Рабочий номер",
+                                 "0 - Отмена"]
         for string in strings_for_edit_menu:
             print(string)
         existing_responses = [char for string in strings_for_edit_menu for char in string if char.isdigit()]
