3cf1acced9bfd02b31547fba12171faa54b96d4d
Alexandr Lisimenko <lisimenko.alexander@yandex.ru>
2023-05-26 16:17:30 +0300

    update contacts.py

--- contacts.py
+++ contacts.py
@@ -1,5 +1,5 @@
 import db
-import UI
+
 
 class ContactRecord:
     def __init__(self, id: int,surname: str, name: str, father_name: str, email: str):
@@ -9,11 +9,11 @@ class ContactRecord:
         self.father_name = father_name
         self.email = email
 
-class NumberRecord:
-    def __init__(self, number: str, type: str, id_contact: int):
-        self.number = number
-        self.type = type
-        self.id_contact = id_contact
+# class NumberRecord:
+#     def __init__(self, number: str, type: str, id_contact: int):
+#         self.number = number
+#         self.type = type
+#         self.id_contact = id_contact
 
 
 def add_contact(contact_rec: ContactRecord):
@@ -27,16 +27,16 @@ def add_contact(contact_rec: ContactRecord):
     cursor.close()
     return id
 
-def add_number(number_rec: NumberRecord):
-    """Добавляет в таблицу numbers"""
-    phone_book = db.get_db()
-    cursor = phone_book.cursor()
-    val_number = [number_rec.number, number_rec.type, number_rec.id_contact]
-    cursor.execute("INSERT INTO numbers(number, type, id_contact) VALUES (?, ?, ?);", val_number)
-    phone_book.commit()
-    id = cursor.lastrowid
-    cursor.close()
-    return id
+# def add_number(number_rec: NumberRecord):
+#     """Добавляет в таблицу numbers"""
+#     phone_book = db.get_db()
+#     cursor = phone_book.cursor()
+#     val_number = [number_rec.number, number_rec.type, number_rec.id_contact]
+#     cursor.execute("INSERT INTO numbers(number, type, id_contact) VALUES (?, ?, ?);", val_number)
+#     phone_book.commit()
+#     id = cursor.lastrowid
+#     cursor.close()
+#     return id
 
 def del_contact(id: int):
     phone_book = db.get_db()
@@ -64,12 +64,12 @@ def find_contact(value=None)-> list[ContactRecord]:
         contacts.append(ContactRecord(contact[0], contact[1], contact[2], contact[3], contact[4]))
     return contacts
 
-def find_numbers(id: int)-> list:
-    phone_book = db.get_db()
-    cursor = phone_book.cursor()
-    cursor.execute("SELECT * FROM numbers WHERE id_contact = ?;", [id])
-    numbers = []
-    for number in cursor.fetchall():
-        numbers.append(number)
-    return numbers
+# def find_numbers(id: int)-> list:
+#     phone_book = db.get_db()
+#     cursor = phone_book.cursor()
+#     cursor.execute("SELECT * FROM numbers WHERE id_contact = ?;", [id])
+#     numbers = []
+#     for number in cursor.fetchall():
+#         numbers.append(number)
+#     return numbers
 
